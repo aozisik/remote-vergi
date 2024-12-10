ab3cd340ee7c68becc6177568a62dd548a338f47
Ahmet Özisik <ahmet@swiftmade.co>
2024-12-10 15:59:06 +0200

    make sections collapsible

--- src/result.ts
+++ src/result.ts
@@ -1,24 +1,32 @@
 import { Dinero } from 'dinero.js';
 import { toText } from './support/money';
 
-export type ResultLine = [string, string | null, string?];
+export type ResultLine = [string, string | null, string | null, string | null, string | null];
 
 export class Result {
   private lines: ResultLine[] = [];
 
-  addLine(label: string, amount: string | Dinero, url?: string) {
+  addLine(
+    label: string,
+    amount: any = null,
+    url: string | null = null,
+    group: string | null = null,
+    position: string | null = null
+  ) {
     this.lines.push([
       label,
-      typeof amount === 'string' ? amount : toText(amount),
+      amount ? amount.toFormat('$0,0') : null,
       url,
+      group,
+      position
     ]);
   }
 
-  addTitle(label: string) {
-    this.lines.push([label, null]);
+  addTitle(title: string) {
+    // Titles are no longer needed as they're handled by the UI
   }
 
-  getLines() {
+  getLines(): ResultLine[] {
     return this.lines;
   }
 }
